ffa0dc8591a638e98041174f6100ccf2e6747c6b
CarmenBene <camy_marya2001@yahoo.com>
2022-02-11 11:46:00 +0200

    added Scroll container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import SearchBox from './SearchBox';
+import App from './containers/App';
 import 'tachyons';
 
 ReactDOM.render(
